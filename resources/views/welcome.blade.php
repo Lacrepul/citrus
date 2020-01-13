cf93861f7e0cb50173f3c8318d2d7b7180e61271
Lacrepul <Poul.Edu@yandex.ru>
2020-01-13 17:29:11 +0300

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div>
+    @auth
+        <div class="container">
+<div class="alert alert-primary">        
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
+                        <div>
+                            namesdsdsaddasdsadasdasdasdasd
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div id="checkListDescription">
+                        dssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    @endauth
+</div>
+@endsection
